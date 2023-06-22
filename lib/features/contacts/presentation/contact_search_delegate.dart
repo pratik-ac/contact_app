54569a05c88e05e24b2d8b0cdb8acab0a3000117
pratik <pra7ik@outlook.com>
2023-06-22 22:21:19 +0545

    second commit

--- lib/features/contacts/presentation/contact_search_delegate.dart
+++ lib/features/contacts/presentation/contact_search_delegate.dart
@@ -1,27 +1,68 @@
+import 'package:contact_app/features/contacts/presentation/my_contacts.dart';
 import 'package:flutter/material.dart';
+import '../data/contacts.dart';
 
 class ContactSearchDelegate extends SearchDelegate<String> {
+  List<Contacts>frnlist = MyContacts().frnDetail;
   @override
   List<Widget>? buildActions(BuildContext context) {
-    // TODO: implement buildActions
-    throw UnimplementedError();
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    // TODO: implement buildLeading
-    throw UnimplementedError();
+    return IconButton(
+      onPressed: () {
+        close(context, '');
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
+    List<Contacts> matchQuery = [];
+
+    for (var fname in frnlist) {
+      if (fname.name.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fname);
+      }
+    }
+
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(matchQuery[index].name),
+        );
+      },
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // TODO: implement buildSuggestions
-    throw UnimplementedError();
+    List<String> matchQuery = [];
+
+    for (var fname in frnlist) {
+      if (fname.name.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fname.name);
+      }
+    }
+
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(matchQuery[index]),
+        );
+      },
+    );
   }
 }
